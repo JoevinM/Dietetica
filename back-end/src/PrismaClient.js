76496c1587626b43bc95948259180f24b18f06f6
CoqAntoine <11534@holbertonstudents.com>
2026-02-11 15:10:11 +0100

    fix: update PrismaClient to correspond to new architecture

--- back-end/src/PrismaClient.js
+++ back-end/src/PrismaClient.js
@@ -1,4 +1,12 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
+import pg from "pg";
 
-const prisma = new PrismaClient()
-export default prisma
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new pg.Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
