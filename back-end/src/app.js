f4855508be1ca6ca9fd0d1c938cc59683953637c
CoqAntoine <11534@holbertonstudents.com>
2026-02-12 16:28:47 +0100

    feat: routes for daily entry and appointment

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,14 +1,18 @@
 import express from "express";
 import cors from "cors";
 
-import userRoutes from './routes/UserRoutes.js';
+import userRoute from './routes/UserRoutes.js';
+import dailyEntryRoute from "./routes/DailyEntryRoute.js";
+import appointmentRoute from "./routes/AppointmentRoute.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/users', userRoutes);
+app.use('/users', userRoute);
+app.use("/daily", dailyEntryRoute);
+app.use("/appointments", appointmentRoute);
 
 app.use((req, res, next) => {
   res.status(404).json({ message: "Route non trouvée" });
