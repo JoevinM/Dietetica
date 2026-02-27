0eb8db2fa96e09e70a8e934b5b02c2067fd1cad6
CoqAntoine <11534@holbertonstudents.com>
2026-02-27 15:40:16 +0100

    feat: routes to protect user to go on diet profil and diet to go on user profil

--- front-end/src/components/ProtectedRoute.jsx
+++ front-end/src/components/ProtectedRoute.jsx
@@ -1,6 +1,35 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 
+// Route accessible uniquement aux utilisateurs classiques
+export function UserRoute({ children }) {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return <div>Loading...</div>; // attend que l'auth soit vérifiée
+  }
+  
+  if (!user)
+    return <Navigate to="/login"   replace />;
+  if (user.role !== "user") return <Navigate to="/dashboard" replace />;
+    return children;
+}
+
+// Route accessible uniquement aux diététiciens
+export function DietRoute({ children }) {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return <div>Loading...</div>; // attend que l'auth soit vérifiée
+  }
+  
+  if (!user)
+    return <Navigate to="/login" replace />;
+  if (user.role !== "dietician" && user.role !=="admin") return <Navigate to="/profil" replace />;
+  return children;
+}
+
+// Route générique
 export default function ProtectedRoute({ children }) {
   const { user, loading } = useAuth();
 
