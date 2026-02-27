716a7ed138fbf071eb98cf2f1263a22dbe0ae92b
CoqAntoine <11534@holbertonstudents.com>
2026-02-27 15:38:06 +0100

    fix: factorize function for clarity

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,31 +1,40 @@
 import { Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Home from "./Home";
-import Login from "./Login";
+import { AuthProvider, useAuth } from "./context/AuthContext";
+import ProtectedRoute, { UserRoute, DietRoute } from "./components/ProtectedRoute";
+import Home from "./components/Home";
+import Login from "./components/Login";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import UserPage from "./components/UserPage/index";
+import DietDashboard from "./components/DietDashboard/index";
 import NewLetter from "./components/NewLetter";
 import NewLetterDetail from "./components/NewLetterDetail";
 
-function App() {
-  return (
-    <AuthProvider>
+// Appelle toutes les routes
+function AppRoutes() {
+  const { user } = useAuth();
+
+  return (  
+    <>
       <Header />
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
-		<Route path="/newletter" element={<NewLetter />} />
-		<Route path="/newletter/:id" element={<NewLetterDetail />} />
+        <Route path="/login"           element={<Login />} />
+        <Route path="/"                element={<ProtectedRoute><Home /></ProtectedRoute>} />
+        <Route path="/profil"          element={<UserRoute><UserPage user={user} /></UserRoute>} />
+        <Route path="/dashboard"       element={<DietRoute><DietDashboard /></DietRoute>} />
+        <Route path="/newletter"       element={<NewLetter />} />
+        <Route path="/newletter/:id"   element={<NewLetterDetail />} />
       </Routes>
       <Footer />
-    </AuthProvider>
+    </>
   );
 }
 
-export default App;
+// App ne fait qu'envelopper avec le Provider
+export default function App() {
+  return (
+    <AuthProvider>
+      <AppRoutes />
+    </AuthProvider>
+  );
+}
