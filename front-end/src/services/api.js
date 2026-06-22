ace4006bbc4a46d6568a003fb6ef8221506c5bfd
joevinM <joevin.manceau@hotmail.fr>
2026-06-22 15:35:17 +0200

    fix: correct baseURL syntax in API service configuration

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: $VITE_API_URL,
-  withCredentials: true // permet de lire le cookie
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true
 });
 
 export default api;
