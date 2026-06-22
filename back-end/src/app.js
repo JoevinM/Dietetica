bc9442ebe4ab962ce9b0f9e914357faa03520a27
joevinM <joevin.manceau@hotmail.fr>
2026-06-22 14:56:36 +0200

    fix: update CORS configuration to allow all origins for API access

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173", // URL frontend
+    origin: *, // permet à toutes les origines d'accéder à l'API
     credentials: true                   // permet de lire le cookie
 }));
 
